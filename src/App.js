6063014c2a2ff60f605d9f58655d4e0daea2613c
Sahil Singh <sahils091@gmail.com>
2021-01-06 14:21:42 -0500

    Created and styled the Input form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import { Container } from "react-bootstrap";
+import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
     </div>
   );
 }
